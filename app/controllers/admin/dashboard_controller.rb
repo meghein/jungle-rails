225c8401355a0804d39ce018b14944b3c06783ba
Meghan Hein <meghan.hein@gmail.com>
2020-07-22 22:01:05 +0000

    create dashboard-counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD']
   def show
+    @products_count = Product.count(:all)
+    @categories_count = Category.count(:all)
   end
 end
+
+
+
