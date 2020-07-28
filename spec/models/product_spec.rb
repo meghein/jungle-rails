c501d9949ed6b5982e204a0115d93407fd0016b1
Meghan Hein <meghan.hein@gmail.com>
2020-07-28 16:24:24 +0000

    user specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -10,52 +10,52 @@ RSpec.describe Product, type: :model do
         price: 5.00,
         quantity: 12,
         category_id: @category.id
-        )
-      expect(product).to (be_valid)
+      )
+      expect(product).to be_valid
     end
 
     it 'should not save without valid name' do
       @category = Category.create(name: "Food")
-      name = Product.new(
+      product = Product.new(
         name: nil,
         price: 5.00,
         quantity: 12,
         category_id: @category.id
-        )
-      expect(name).to_not (be_valid)
+      )
+      expect(product).to_not be_valid
     end
 
     it 'should not save without valid price' do
       @category = Category.create(name: "Food")
-      price = Product.new(
+      product = Product.new(
         name: "Tofu",
         price: nil,
         quantity: 12,
         category_id: @category.id
-        )
-      expect(price).to_not (be_valid)
+      )
+      expect(product).to_not be_valid
     end
 
     it 'should not save without valid quantity' do
       @category = Category.create(name: "Food")
-      quantity = Product.new(
+      product = Product.new(
         name: "Tofu",
         price: 5.00,
         quantity: nil,
         category_id: @category.id
-        )
-      expect(quantity).to_not (be_valid)
+      )
+      expect(product).to_not be_valid
     end
 
     it 'should not save without valid category id' do
       @category = Category.create(name: "Food")
-      category_id = Product.new(
+      product = Product.new(
         name: "Tofu",
         price: 5.00,
         quantity: 12,
         category_id: nil
-        )
-      expect(category_id).to_not (be_valid)
+      )
+      expect(product).to_not be_valid
     end
   end
 end
